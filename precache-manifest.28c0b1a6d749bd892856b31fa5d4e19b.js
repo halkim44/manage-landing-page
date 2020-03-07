375eb06ae8217351c924c44c11fb71b1e4bc60b7
Halkim <kimketchum15@gmail.com>
2020-03-07 23:47:29 +0800

    Updates

---
+++ precache-manifest.28c0b1a6d749bd892856b31fa5d4e19b.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b0dbde73f968583690cfce5b683b557b",
+    "url": "./index.html"
+  },
+  {
+    "revision": "8e8b88c354105ea3afa6",
+    "url": "./static/css/main.36c35e44.chunk.css"
+  },
+  {
+    "revision": "18eeb71367a27a043b53",
+    "url": "./static/js/2.a44c419b.chunk.js"
+  },
+  {
+    "revision": "60ecaad85320638366e6b61932d38edb",
+    "url": "./static/js/2.a44c419b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8e8b88c354105ea3afa6",
+    "url": "./static/js/main.5ce1634f.chunk.js"
+  },
+  {
+    "revision": "16a17a183c7db7b85423",
+    "url": "./static/js/runtime-main.c17e7065.js"
+  },
+  {
+    "revision": "d022b688c087eb4bc54fa88f43ccb1be",
+    "url": "./static/media/avatar-ali.d022b688.png"
+  },
+  {
+    "revision": "d591dc600c0ee916d50094bdc81d8902",
+    "url": "./static/media/avatar-anisha.d591dc60.png"
+  },
+  {
+    "revision": "a2c4b4309f9936736240c6d7805c7ccf",
+    "url": "./static/media/avatar-richard.a2c4b430.png"
+  },
+  {
+    "revision": "b2507cb2cb85266d8f2051f637b744aa",
+    "url": "./static/media/avatar-shanai.b2507cb2.png"
+  },
+  {
+    "revision": "010617910cf89bbbf2df1f8bf2340912",
+    "url": "./static/media/bg-tablet-pattern.01061791.svg"
+  },
+  {
+    "revision": "77f24c26f2fbe988a55b27f318c8ff32",
+    "url": "./static/media/icon-facebook.77f24c26.svg"
+  },
+  {
+    "revision": "e0814599d1e638a04578e79a652801c0",
+    "url": "./static/media/icon-instagram.e0814599.svg"
+  },
+  {
+    "revision": "ef15af23a9c6edf1014f6f44b31c8b78",
+    "url": "./static/media/icon-pinterest.ef15af23.svg"
+  },
+  {
+    "revision": "064c31714c77b6580c3e4e8176c435fb",
+    "url": "./static/media/icon-twitter.064c3171.svg"
+  },
+  {
+    "revision": "8e2f566fcefd23c8aacdaa5a83cdaff2",
+    "url": "./static/media/icon-youtube.8e2f566f.svg"
+  },
+  {
+    "revision": "6a53bba8095e0ddefa1791bbd03cf51f",
+    "url": "./static/media/illustration-intro.6a53bba8.svg"
+  },
+  {
+    "revision": "b3430221dc0fe34fdb947df41cfab7dc",
+    "url": "./static/media/logo-white.b3430221.svg"
+  },
+  {
+    "revision": "f2cec4b29d7e7f388db937e3f5d9d873",
+    "url": "./static/media/logo.f2cec4b2.svg"
+  }
+]);
